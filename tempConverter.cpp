49bd2a209f6a96a36cc1925635801660144c94ef
EmcD123 <mcdonnell.owen1@gmail.com>
2017-01-18 09:06:23 +0000

    Upgrading the temperature converter programme to learn loops,functions,decisions

--- tempConverter.cpp
+++ tempConverter.cpp
@@ -2,19 +2,60 @@
 
 #include <iostream>
 
+//To remove nessecity of std::
 using namespace std;
 
-
-int tempConverter(float celsius){
+//function to convert temperatures from Clesius to Fahrenheit
+int CF_Converter(){
   float fahrenheit;
+  float celsius;
+  string s;
+  cout << "Please input temperature in degrees celsius \n" ;
+  cin >> celsius;
   fahrenheit = ((9.0/5.0)*celsius)+ 32;
   return fahrenheit;
 }
 
+int FC_Converter(){
+  float fahrenheit;
+  float celsius;
+  string s;
+  cout << "Please input temperature in degrees fahrenheit \n" ;
+  cin >> fahrenheit;
+  celsius = (fahrenheit - 32) / (9.0/5.0);
+  return celsius;
+}
+
+
 int main(){
-  float temp;
-  temp = tempConverter(20.1);
-  cout << "Temperature in fahrenheit is " << temp << " degrees\n";
+
+  int input;
+
+  cout << "This is a temperature converter \n";
+  cout << "For celsisus fahrenheit conversion type '0' \n";
+  cout << "For fahrenheit to celsius conversion type '1' \n";
+  cin >> input;
+
+  //while (!(input == 0 || input == 1)) {
+  //  cin >> input;
+    //if (input != 0 || input != 1) {
+      //  cout << "Not a valid input,try again";
+        //cin >> input;
+    //}
+  //}
+
+  if(input == 0){
+    float CF;
+    CF = CF_Converter();
+    cout << "Temperature in fahrenheit is " << CF << " degrees\n";
+  }
+  if (input == 1){
+    float FC;
+    FC = FC_Converter();
+    cout << "Temperature in celsius is " << FC << " degrees\n";
+  }
+
 
   return 0;
+
 }
