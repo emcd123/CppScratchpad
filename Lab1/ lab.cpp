24ed709dcd85e6f03dc90024a939706ca19db814
EmcD123 <mcdonnell.owen1@gmail.com>
2017-01-21 21:18:06 +0000

    Sci comp lab work

---
+++ Lab1/ lab.cpp
@@ -0,0 +1,27 @@
+//A c++ programme containing the
+//individual questions in the online lab sheet
+
+#include <iostream>
+
+using namespace std;
+
+int Q1(){
+  //Q1 from the lab sheet
+  //Define a variable
+  //initialise it with value 9
+  int a;
+  a = 9;
+  return a;
+}
+
+
+
+
+int main(){
+  //Executing question 1
+  int Q1;
+  Q1 = Q1();
+
+
+  return 0;
+}
