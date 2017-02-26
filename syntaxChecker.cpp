516eefaf92b051097a677a5850ddb89dced7219c
EmcD123 <mcdonnell.owen1@gmail.com>
2017-02-26 20:28:22 +0000

    Basically done

--- syntaxChecker.cpp
+++ syntaxChecker.cpp
@@ -3,13 +3,59 @@
   Written by: EmcD123
 */
 
+/******************************************************************************************/
+/*                    ChangeLog                                                           */
+/* Fixed:  Top function correctly returns top of stack                                    */
+/*         Brackets now accurately match with each other                                  */
+/*         Brackets in strings and regular '//' style comments are ignored                */
+/*         C-style comments(leave until last)                                             */
+/*         Error not called when closed bracket exist without an open bracket             */
+/*         Have to fix "No errors" message when a closing bracket without opening exists  */
+
+/* To Do:                                                                                 */
+/*         Checking order of brackets                                                     */
+/*         Line Count                                                                     */
+/*         Implement a isComment() function to remove excess if statements from main()    */
+/*         Need to improve effectiveness of output                                        */
+/******************************************************************************************/
+
+/**************************************************************************************************************
+                     Instructions
+    Stack class is same as the stack in previous assignment, just makes a stack to push and pop from
+    It has a new member function called topCheck() ,which finds the top element of the stack and returns it
+
+    The bracket matching is done using two functions; openBracketMatch() and closedBracketMatch()
+    openBracketMatch() checks if a given character is one of (, [, { and if it is pushes it to the stack
+    closedBracketMatch() checks if a given character is one of ), ], } and it matches the top of the stack,
+    if it does then it pops the open bracket off the stack
+
+    The third bracket function, isCloseBracket(), just checks that c is a closing bracket, that way
+    we can return an error if there isnt a bracket on the stack to match with like in closedBracketMatch()
+
+    For '//' we check if the first char is /, then use std::peak to see the next char
+    If we are in a comment we keep increment through chars until we reach a new Line
+    Then we start bracket checking again
+
+    Strings of type "" and '' are treated much the same as above
+
+    At the end, we check if the stack is empty; if it is then we pop each element off it and
+    return an error saying what type of bracket is missing in the code.
+
+    Flag variable works as a checkbit. Due to the design of my push,pop procedure, when a closing bracket was found
+    without an opener on the stack the program while pointing out the error would still say "No errors found"
+    Now if that error appears it switches flag from 0 to 1 to prevent the "No errors found" message
+
+    If at the end, the stack is empty; Then there are no errors this code can detect in the file;
+    So we return a "No errors in code" message
+************************************************************************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #define MAX_STACK 10
 
 bool openBracketMatch(char c);
-bool isComment(char k, char c);
+bool isCloseBracket(char c);
 
 class stack {
 private:
@@ -47,7 +93,7 @@ bool stack::emptyCheck(void) {
   }
 }
 bool stack::fullCheck(void){
-  if(top == (StackSize)){
+  if(top == (StackSize)){//stack is full
     return true;
   }
   else{
@@ -73,9 +119,8 @@ char stack::pop(void){
       return(contents[top]);
     }
 }
-char stack::topCheck(void){
+char stack::topCheck(void){//return the character on top of stack
     if(emptyCheck()){
-      //std::cerr << "An opening bracket is missing somewhere";
       return((char)NULL);
     }
     else{
@@ -90,6 +135,10 @@ int main(){
   std::ifstream InFile;
   std::ofstream OutFile;
   char c;
+  int flag = 0;//similar to a check bit
+  int lineCount = 0;
+  int pushCount = 0;
+  int popCount = 0;
 
   std::string file;
   std::cout<< "Enter name of file to be checked: "<< std::endl;
@@ -107,37 +156,46 @@ int main(){
     i++;
     InFile.get(c);
 
-    /*
-      Procedure: each character is gotten until end of file is reached
-      Then check if that character is a [,],{,},(,)
-      if it is one of those, push onto the stack
-      find you find its matching character pop it off the stack,
-      At the end, if the stack is not empty there is an error
-    */
-    if(c == '/' && InFile.peek() == '/'){
+    if(c == '\n'){
+      lineCount++;
+    }
+    if(c == '/' && InFile.peek() == '/'){//Check if we're in a '//' style comment,skip on until '\n'
       while(c != '\n'){
         InFile.get(c);
       }
     }
-    if(c == ('\'')){
-//      InFile.get(c);
-      while(c != '\''){
+    if(c == '/' && InFile.peek() == '*'){//same as above but for c-style comments
+      InFile.get(c);
+      InFile.get(c);
+      while(c != '*' && InFile.peek() != '/'){
         InFile.get(c);
       }
     }
-    if(c == ('\"')){
+    if(c == ('\'')){ //same as above but for characters
+      InFile.get(c);
+      InFile.get(c);
+    }
+    if(c == ('\"')){//same as above but for strings
       InFile.get(c);
       while(c != '\"'){
         InFile.get(c);
       }
     }
-    if(openBracketMatch(c)){//finds if a character is a bracket,returns true then push character
+    if(openBracketMatch(c)){//finds if a character is a opening bracket,if returns true then push character
       syntaxChecker.push(c);
+      pushCount++;
+      //std::cout << "Pushing: " << c << std::endl;
     }
-
-    if(closedBracketMatch(syntaxChecker,c)){//finds if a character is a bracket,returns true then pop character
-      std::cout << "Popping: " << syntaxChecker.pop() << std::endl;
-      //syntaxChecker.pop();
+    if(isCloseBracket(c)){//finds if a character is a closing bracket,then check it matches top,then pop character
+        if(closedBracketMatch(syntaxChecker,c)){
+          //std::cout << "Popping: " << syntaxChecker.pop() << std::endl;
+          syntaxChecker.pop();
+          popCount++;
+        }
+        else{
+          flag  = 1;
+          std::cout << "Match of type " << c << " is needed" << std::endl;
+        }
     }
   }
   OutFile << file << "file contains "
@@ -146,23 +204,24 @@ int main(){
   InFile.close();
   OutFile.close();
 
-  if(syntaxChecker.emptyCheck()){
+   std::cout << "Lines checked: " << lineCount << std::endl;
+  if(flag == 0 && syntaxChecker.emptyCheck()){
     std::cout << "No errors found" << std::endl;
   }
-
-  while(! syntaxChecker.emptyCheck()){
-    std::cout << "The match for this type bracket is missing somewhere: ";
+  //Show the amount of opening/closing brackets found/expected
+  std::cout << "Brackets checked: " << pushCount;
+  std::cout << " Brackets found: "<< popCount << std::endl;
+  //check if there is weighting issues in the brackets
+  if(popCount != pushCount){
+    std::cout << "Possible error(s) detected: see errors to identify" << std::endl;
+    std::cout << "Error may also be caused by brackets entered in wrong order." << std::endl;
+  }
+  while(! syntaxChecker.emptyCheck()){//Empty the stack at the end of programme
+    std::cout << "Bracket error detected,match for this bracket needed: ";
     std::cout << syntaxChecker.pop() << std::endl;
+    popCount++;
   }
   return 0;
-}
-bool isComment(char k, char c){
-  if(k == '/' && c == '/'){
-    return true;
-  }
-  else{
-    return false;
-  }
 }
 bool openBracketMatch(char c){
   if(c == '('){
@@ -172,6 +231,20 @@ bool openBracketMatch(char c){
     return true;
   }
   if(c == '{'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+bool isCloseBracket(char c){
+  if(c == ')'){
+    return true;
+  }
+  if(c == ']'){
+    return true;
+  }
+  if(c == '}'){
     return true;
   }
   else{
@@ -193,19 +266,3 @@ bool closedBracketMatch(stack &syntaxChecker ,char c){
     return false;
   }
 }
-/*
-bool closedBracketMatch(char k, char c){
-  if(k == '(' && c == ')'){
-    return true;
-  }
-  if(k == '[' && c == ']'){
-    return true;
-  }
-  if(k == '{' && c == '}'){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-*/
