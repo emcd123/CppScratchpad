08892d09cb9e91d5c727e205db8336a52c3994e4
emcd123 <mcdonnell.owen1@gmail.com>
2017-09-18 10:09:40 +0100

    Misc

---
+++ primeFactorization.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>       /* sqrt */
+
+int isPrime(intx);
+int isFactor(int x);
+
+int main(){
+
+
+
+
+}
+
+
+bool isPrime(int x){
+	for(int j=0; j<
+		if( x%j == 0){
+			return true;
+		else{
+			return false;
+		}
+}
+
+int isFactor(int x){
+	
+
+}
+
+
+main();
