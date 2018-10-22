145f251341048568080f625d7d6534d457a2c698
emcd123 <mcdonnell.owen1@gmail.com>
2018-10-22 15:49:14 +0100

    BubbleSort working, need to refactor

--- Other/sorting.cpp
+++ Other/sorting.cpp
@@ -1,59 +1,57 @@
-//C++ program to implement
-//various sorting techniques
+//g++  5.4.0
 
 #include <iostream>
 #include <stdlib.h> //To use random number functions
 
 using namespace std;
 
-//1st ALGORITHM
-//Bubble sorting
-//Complexity O(n^2)
-char Bubble(){
+#define MAX_SIZE 10
+class BubbleSort{
+ public:
+    void Swap(int &p1, int &p2 );
+    void Sort( int* array ,int arrSize);
+    BubbleSort(void);
+    ~BubbleSort(void);
+};
+BubbleSort::BubbleSort(void){
+}
+
+BubbleSort::~BubbleSort(){
+}
+void BubbleSort::Sort(int* array,int arrSize){
+  for(int i=(arrSize-1); i>1; i--){
+    for(int k=0; k<i; k++){
+      if(array[k] > array[k+1]){
+        swap(array[k], array[k+1]);
+      }
+    }
+  }
+}
+
+void BubbleSort::Swap(int &p1, int &p2) {
+     int temp = p1;
+     p1 = p2;
+     p2 = temp;
+}
+int main()
+{
   const int n = 5;
   int array[n];
-
   /* initialize random seed: */
   srand (time(NULL));
   //populate array with random integers
   for(int i = 0; i< n; i++){
     array[i] = rand() %10;
-  }
-  cout << "Unsorted random generted list: ";
+   }
+   
+     cout << "Unsorted random generted list: ";
   for(int j= 0; j< n; j++){
     cout << array[j];
-  }
-  cout << "\n";
-  for(int i = (n-1); i>1; i--){
-    for(int k = 0; k < i; k++){
-    //loop through array,
-    //check if k > k+1
-    //if so swap the index of k and k+1
-      if(array[k] > array[k+1]){
-      //void swap(int &p1, int &p2);
-        swap(array[k], array[k+1]);
-      }
-    }
-  }
-  cout << "Sorted random generted list: ";
+   }
+   BubbleSort bsort;
+   bsort.Sort(array, n);
+     cout << "Sorted random generted list: ";
   for(int j= 0; j< n; j++){
     cout << array[j];
-  }
-
-  return 'e';
- }
-
-void swap(int &p1, int &p2) {
-   //if ((p1 != NULL) && (p2 != NULL)) {
-     int temp = p1;
-     p1 = p2;
-     p2 = temp;
-  //}
-}
-
-int main(){
-  //bubble;
-  //bubble = Bubble();
-  cout << Bubble();
-  return 0;
+   }
 }
